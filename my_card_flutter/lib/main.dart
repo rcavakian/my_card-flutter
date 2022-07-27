2e4caca4931e761e0c63c59ffd59250c6e7d0be2
Ricardo Camillo Avakian <ravakian@fapex.org.br>
2022-07-27 11:55:23 -0300

    first commit criacao do projeto Meu Card Flutter curso Polimorfismo

---
+++ my_card_flutter/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MeuApp(),
+  );
+}
+
+class MeuApp extends StatelessWidget {
+  const MeuApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.redAccent,
+        body: Container(),
+      ),
+    );
+  }
+}
