003ceac41470a3165b95ef7792ad8e21a3a00f4a
Ricardo Camillo Avakian <ravakian@fapex.org.br>
2022-07-28 09:11:43 -0300

    first commit criacao do projeto Meu Card Flutter curso Polimorfismo aula 14: usando o widget Container

--- my_card_flutter/lib/main.dart
+++ my_card_flutter/lib/main.dart
@@ -14,8 +14,17 @@ class MeuApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.redAccent,
-        body: Container(),
+        backgroundColor: Colors.pinkAccent,
+        body: SafeArea(
+          child: Container(
+            margin: const EdgeInsets.only(left: 25, top: 25),
+            padding: const EdgeInsets.all(10),
+            height: 100,
+            width: 100,
+            color: Colors.amber,
+            child: const Text('Olá pessoal !'),
+          ),
+        ),
       ),
     );
   }
