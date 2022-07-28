59f510b968f225ce874b8da9a5071c6ddce029c3
Ricardo Camillo Avakian <ravakian@fapex.org.br>
2022-07-28 11:40:23 -0300

    atualizacao do projeto com aula 15

--- my_card_flutter/lib/main.dart
+++ my_card_flutter/lib/main.dart
@@ -16,13 +16,31 @@ class MeuApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.pinkAccent,
         body: SafeArea(
-          child: Container(
-            margin: const EdgeInsets.only(left: 25, top: 25),
-            padding: const EdgeInsets.all(10),
-            height: 100,
-            width: 100,
-            color: Colors.amber,
-            child: const Text('Olá pessoal !'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(10),
+                height: 100,
+                width: 100,
+                color: Colors.amber,
+                child: const Text('Container 1'),
+              ),
+              Container(
+                padding: const EdgeInsets.all(10),
+                height: 100,
+                width: 100,
+                color: Color.fromARGB(255, 255, 106, 7),
+                child: const Text('Container 2'),
+              ),
+              Container(
+                padding: const EdgeInsets.all(10),
+                height: 100,
+                width: 100,
+                color: Color.fromARGB(255, 255, 7, 7),
+                child: const Text('Container 3'),
+              ),
+            ],
           ),
         ),
       ),
